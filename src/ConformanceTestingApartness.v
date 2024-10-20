4b8fef018430d17cbf284a55563e7f83ee57b7a8
YouGuessedMyName <ivo@melse.info>
2024-10-20 11:54:51 +0200

    progress

--- src/ConformanceTestingApartness.v
+++ src/ConformanceTestingApartness.v
@@ -2,6 +2,7 @@
 (* Conformance Testing Reconsidered *)
 
 Require Import Coq.Sets.Finite_sets_facts.
+Require Import Coq.Lists.List.
 
 (* Enable classical logic *)
 (* Sorry Freek, I know you love propositional logic*)
@@ -22,20 +23,31 @@ Inductive O := oa | ob.
 
 Definition word := list.
 
+(* v is a prefix of w *)
+Definition pref (v w : word I) : Prop := exists v' : word I, v ++ v' = w.
+(* A is the set of prefixes of set B *)
+Definition Pref (A B : word I -> Prop) := 
+forall a b : word I, A a /\ B b <-> pref a b.
+
+
 (* Definition 2.1 (Mealy machine) *)
 (* Here, we deviate a bit from the paper. Instead of having two transitions functions, we combine them into one *)
+Definition Y := word I.
 Structure Mealy : Type := {
-  Q : Set;
-  q0 : Q;
-  trans : Q -> I -> option (Q * O);
+  q0 : Y;
+  trans : Y -> I -> option (Y * O);
+  Q : Y -> Prop;
 }.
 
-Definition delta {M : Mealy} (q : Q M) (i : I) : option (Q M) :=
+(* The intial state is always in the states *)
+Parameter initial : forall M : Mealy, Q M (q0 M).
+
+Definition delta (M : Mealy) (q : Y) (i : I) : option (Y) :=
 match (trans M q i) with
   | None => None
   | Some (r, _) => Some r
 end.
-Definition lambda {M : Mealy} (q : Q M) (i : I) : option O :=
+Definition lambda (M : Mealy) (q : Y) (i : I) : option O :=
 match (trans M q i) with
   | None => None
   | Some (_, o) => Some o
@@ -46,34 +58,34 @@ end.
 Parameter lambda_delta2: forall M : Mealy, forall i : I, forall q : Q M, 
   (delta M q i ↑) <-> (lambda M q i ↑). *)
 
-Definition complete {M : Mealy} (q : Q M) :=
+Definition complete (M : Mealy) (q : Y) :=
   forall i : I, ((trans M q i) ↓).
-Definition completeSet {M : Mealy} (W : Q M -> Prop) :=
-  forall q : Q M,
-    W q -> complete q.
+Definition completeSet {M : Mealy} (W : Y -> Prop) :=
+  forall q : Y, Q M q ->
+    W q -> complete M q.
 Definition completeM (M : Mealy) : Prop
-  := forall q : Q M, complete q.
+  := forall q : Y, Q M q -> complete M q.
 (* Lift delta and lambda to sequences *)
-Fixpoint transS {M : Mealy} (q: Q M) (v : word I) : (option ((Q M) * word O)) :=
+Fixpoint transS (M : Mealy) (q: Y) (v : word I) : (option ((Y) * word O)) :=
 match v with
   | nil => Some (q, nil)
   | cons i v' => 
     match (trans M q i) with
      | Some (r, o) => 
-        match (transS r v') with
+        match (transS M r v') with
           | None => None
           | Some (r', w) => Some (r', w)
         end
      | None => None
     end
 end.
-Definition deltaS {M : Mealy} (q: Q M) (v : word I) : (option (Q M)) := 
-match (transS q v) with
+Definition deltaS (M : Mealy) (q: Y) (v : word I) : (option (Y)) := 
+match (transS M q v) with
   | Some (r, w) => Some r
   | None => None
 end.
-Definition lambdaS {M : Mealy} (q: Q M) (v : word I) : (option (word O)) := 
-match (transS q v) with
+Definition lambdaS (M : Mealy) (q: Y) (v : word I) : (option (word O)) := 
+match (transS M q v) with
   | Some (r, w) => Some w
   | None => None
 end.
@@ -82,32 +94,32 @@ Notation "q - v w -> r" := (transS q v = (r, w)) (at level 303, no associativity
 Notation "q = v w => r" := (q - v w -> (r, w) /\ length v > 0) (at level 303, no associativity) : type_scope.
 
 Definition stateCover (M : Mealy) (A: (word I) -> Prop) : Prop 
-  := forall q : Q M, exists v : (word I), A v /\ deltaS (q0 M) v = Some q.
+  := forall q : Y, Q M q -> exists v : (word I), A v /\ deltaS M (q0 M) v = Some q.
 Definition minimalStateCover (M : Mealy) (A: (word I) -> Prop) : Prop
-  := stateCover M A /\ forall q : Q M, ~ (exists v w : (word I), A v /\ A w /\ v <> w /\ deltaS (q0 M) v = Some q /\ deltaS (q0 M) w = Some q).
+  := stateCover M A /\ forall q : Y, Q M q -> ~ (exists v w : (word I), A v /\ A w /\ v <> w /\ deltaS M (q0 M) v = Some q /\ deltaS M (q0 M) w = Some q).
 Definition initiallyConnected (M : Mealy) : Prop
   := exists A : ((word I) -> Prop), stateCover M A.
 (* 'We will only consider Mealy machines that are initially connected.' *)
 Parameter allInitiallyConnected : forall M : Mealy, initiallyConnected M.
 
 (* Definition 2.2 (Semantics and minimality) *)
-Definition semantics {M : Mealy} (q : Q M) :=
-  lambdaS q.
+Definition semantics (M : Mealy) (q : Y) :=
+  lambdaS M q.
 Notation "[ q ]" := (semantics q) (at level 303, no associativity) : type_scope. 
-Definition equiv {M N : Mealy} (q : Q M) (r : Q N) : Prop :=
-  semantics q = semantics r.
+Definition equiv (M N : Mealy) (q : Y) (r : Y) : Prop :=
+  semantics M q = semantics M r.
 Notation "q ≈ r" := (equiv q r) (at level 303, no associativity) : type_scope. 
 Definition equivM (M N : Mealy) : Prop :=
-  equiv (q0 M)(q0 N).
+  equiv M N (q0 M)(q0 N).
 Definition minimalM (M : Mealy) : Prop :=
-  forall q r : Q M, (q ≈ r) <-> q <> r.
+  forall q r : Y, Q M q -> Q M r -> (equiv M M q r) <-> q <> r.
 
 (* Definition 2.4 (Simulation) *)
 (* f : M -> N is a funcitonal simulation from M to N *)
-Definition funcSim (M N : Mealy) (f: Q M -> Q N) : Prop :=
+Definition funcSim (M N : Mealy) (f: Y -> Y) : Prop :=
   f(q0 M) = q0 N 
 /\
-  forall q : Q M,  forall i : I, 
+  forall q : Y, Q M q -> forall i : I, 
       match (trans M q i) with
         | None => True
         | Some (r, o) => 
@@ -120,41 +132,110 @@ Definition funcSim (M N : Mealy) (f: Q M -> Q N) : Prop :=
 
 (* Definition 2.5 (Observation tree) *)
 Definition tree (M : Mealy) : Prop
-  := forall q : Q M, ~ (exists v w : (word I), v <> w /\ deltaS (q0 M) v = Some q /\ deltaS (q0 M) w = Some q).
+  := forall v : word I, deltaS M (q0 M) v = Some v.
 
-Definition access {M : Mealy} (v : word I) (q : Q M) : Prop
-  := deltaS (q0 M) v = Some q.
+Definition access (M : Mealy) (v : word I) : word I := v.
 
-Definition accessSet {M : Mealy} (A : word I -> Prop) (U: Q M -> Prop): Prop
-  := forall q : Q M, exists v : word I, A v /\ U q /\ access v q.
+Definition accessSet {M : Mealy} (U: Y -> Prop): (word I -> Prop)
+  := U.
 
 (* q' is the parent of q *)
-Definition parent {M : Mealy} (q q' : Q M) : Prop
-  := exists i : I, delta q' i = Some q.
+Definition parent {M : Mealy} (q q' : Y) : Prop
+  := exists i : I, delta M q' i = Some q.
 
 (* T is an observation tree for M *)
 Definition observationTreeFor (T : Mealy) (M : Mealy) : Prop
-  := exists f : (Q T -> Q M), funcSim T M f.
+  := exists f : (Y -> Y), funcSim T M f.
 
 (* Definition 2.7 (Test suites) *)
 (* v is a test case for S *)
-Definition testCase (v : word I) (S : Mealy) := def (transS (q0 S) v).
+Definition testCase (v : word I) (S : Mealy) := def (transS S (q0 S) v).
 
 (* T is a test suite for S *)
 Definition testSuite (T : word I -> Prop) (S : Mealy) := 
-    Finite _ T /\ forall v : word I, T v -> testCase v S.
+    Finite (word I) T /\ forall v : word I, T v -> testCase v S.
 
 (* M passes test v for S *)
 Definition passes1 (S M : Mealy) (v : word I) :=
-  lambdaS (q0 S) v = lambdaS (q0 M) v.
+  lambdaS M (q0 S) v = lambdaS M (q0 M) v.
 
 (* M passes test suite T for S *)
 Definition passes2 (S M : Mealy) (T : word I -> Prop) :=
   forall v : word I, T v -> passes1 S M v.
 
+Check Union.
+Check Same_set.
+Print Same_set.
+Check Finite.
+Check Ensemble.
+(* Union _ T T = Union _ T T. *)
+(* Same_set _ T T *)
+
 (* TT is a testing tree for test suite T and Mealy machine S *)
-Definition testingTree (TT M : Mealy) (T : word I -> Prop) :=
-  
+Definition testingTree (TT S : Mealy) (T : word I -> Prop) : Prop :=
+tree TT /\
+(* QT = {ϵ} ∪ Pref (T ) and qT0 = ϵ,*)
+(forall A : word I -> Prop, Pref A T -> A = Q TT) /\
+(* For all σ ∈ I∗ and i ∈ I with σi ∈ QT , δT (σ, i) = σi, *)
+(forall v : word I, forall i : I, 
+  Q TT (v ++ cons i nil) ->
+    delta TT v i = Some (v ++ cons i nil)) /\
+(* For all σ ∈ I∗ and i ∈ I with σi ∈ QT , λT (σ, i) = λS (δS (qS0 , σ), i).*)
+(forall v : word I, forall i : I,
+  Q TT (v ++ cons i nil) ->
+    match (deltaS S (q0 S) v) with
+      | None => True
+      | Some w => lambda TT v i = lambda S w i
+    end
+).
+
+(* For every Tree, the root state is nil. *)
+Lemma treeRoot : forall TT : Mealy, tree TT -> q0 TT = nil.
+Proof.
+intros.
+unfold tree in H.
+specialize (H nil).
+unfold deltaS in H.
+unfold transS in H.
+injection H as H.
+apply H.
+Qed.
+
+Lemma lemma_2_9 : forall S TT : Mealy, forall T : word I -> Prop, forall f: Y -> Y,
+        (testingTree TT S T 
+      /\ 
+        (forall v : Y, Q TT v ->
+          match (deltaS S (q0 S) v) with
+            | None => False
+            | Some r => f v = r
+          end))
+  ->
+    funcSim TT S f.
+Proof.
+intros.
+destruct H.
+unfold funcSim.
+split.
+  - specialize (H0 (q0 TT)).
+    rewrite treeRoot in H0.
+    unfold deltaS in H0.
+    unfold transS in H0.
+    rewrite treeRoot.
+    apply H0.
+    rewrite <-treeRoot with TT.
+    apply initial.
+    apply H. apply H. apply H.
+
+  - intros.
+    unfold deltaS in H0.
+    unfold testingTree in H.
+    
+    
+    apply H0.
+
+(* lambda TT v i = lambda S (deltaS S (q0 S) v) i) *)
+Definitino testingTree2 (TT M : Mealy) (T : word I -> Prop) :=
+
 
 Definition SI : (I*O) -> (I*O) -> Prop
    := fun '(i1,_) '(i2,_) => i1 = i2.
